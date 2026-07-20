92c64187b914cd96f40ef0a3792ad0e0f65110e5
Antony Jurjevich <49915351+ko3moc@users.noreply.github.com>
2026-07-20 10:25:44 +0200

    Update Field.php

--- src/Core/Field.php
+++ src/Core/Field.php
@@ -1,1 +1,23 @@
+<?php
 
+declare(strict_types=1);
+
+namespace WP_Restaurant\Core\Fields;
+
+defined('ABSPATH') || exit;
+
+/**
+ * Immutable field definition.
+ */
+final class Field
+{
+    public function __construct(
+        public readonly string $key,
+        public readonly string $label,
+        public readonly string $type,
+        public readonly mixed $default = '',
+        public readonly array $options = [],
+        public readonly array $attributes = []
+    ) {
+    }
+}
